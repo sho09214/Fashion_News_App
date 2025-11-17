03eb62193507bbf71b5d2c922ce6ea5e5e4de274
sho09214 <motohashi.sho@gmail.com>
2025-11-17 22:08:37 +0900

    create project

---
+++ src/main/java/com/example/Fashion_News_App/FashionNewsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fashion_News_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionNewsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionNewsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Fashion_News_App/FashionNewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fashion_News_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionNewsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
